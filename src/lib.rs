15e7ed479bbf7090e1f20dbafc288ae41aa2cd96
Metamate <jakikob@gmail.com>
2021-09-29 22:43:34 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
+
+mod player;
 use bevy::prelude::*;
+use player::PlayerPlugin;
 use wasm_bindgen::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
@@ -35,7 +38,7 @@ impl Default for Speed {
 pub fn run() {
     let mut app = App::build();
 
-        app.insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+    app.insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
             title: "Rust Invaders".to_string(),
             width: 600.0,
@@ -44,18 +47,11 @@ pub fn run() {
         })
         .add_plugins(DefaultPlugins);
 
-        #[cfg(target_arch = "wasm32")]
-        app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
 
-        app
-        .add_startup_system(setup.system())
-        .add_startup_stage(
-            "game_setup_actors",
-            SystemStage::single(player_spawn.system()),
-        )
-        .add_system(player_movement.system())
-        .add_system(player_fire.system())
-        .add_system(laser_movement.system())
+    app.add_startup_system(setup.system())
+        .add_plugin(PlayerPlugin)
         .run();
 }
 
@@ -80,82 +76,4 @@ fn setup(
         w: window.width(),
         h: window.height(),
     });
-}
-
-fn player_spawn(mut commands: Commands, materials: Res<Materials>, win_size: Res<WinSize>) {
-    // spawn a sprite
-    let bottom = -win_size.h / 2.;
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.player_materials.clone(),
-            transform: Transform {
-                translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
-                scale: Vec3::new(0.5, 0.5, 1.),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Player)
-        .insert(PlayerReadyFire(true))
-        .insert(Speed::default());
-}
-
-fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Speed, &mut Transform), With<Player>>,
-) {
-    if let Ok((speed, mut transform)) = query.single_mut() {
-        let dir = if keyboard_input.pressed(KeyCode::Left) {
-            -1.
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            1.
-        } else {
-            0.
-        };
-        transform.translation.x += dir * speed.0 * TIME_STEP;
-    }
-}
-
-fn player_fire(
-    mut commands: Commands,
-    kb: Res<Input<KeyCode>>,
-    materials: Res<Materials>,
-    mut query: Query<(&Transform, &mut PlayerReadyFire), With<Player>>,
-) {
-    if let Ok((player_tf, mut ready_fire)) = query.single_mut() {
-        if ready_fire.0 && kb.pressed(KeyCode::Space) {
-            let x = player_tf.translation.x;
-            let y = player_tf.translation.y;
-            commands
-                .spawn_bundle(SpriteBundle {
-                    material: materials.laser.clone(),
-                    transform: Transform {
-                        translation: Vec3::new(x, y + 15., 0.),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                })
-                .insert(Laser)
-                .insert(Speed::default());
-            ready_fire.0 = false;
-        }
-
-        if kb.just_released(KeyCode::Space) {
-            ready_fire.0 = true;
-        }
-    }
-}
-
-fn laser_movement(
-    mut commands: Commands,
-    win_size: Res<WinSize>,
-    mut query: Query<(Entity, &Speed, &mut Transform), With<Laser>>,
-) {
-    for (laser_entity, speed, mut laser_tf) in query.iter_mut() {
-        let translation = &mut laser_tf.translation;
-        translation.y += speed.0 * TIME_STEP;
-        if translation.y > win_size.h {
-            commands.entity(laser_entity).despawn();
-        }
-    }
 }
